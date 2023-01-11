bbdd9757062f893d2a7cd2102689282d8c935462
Biasem <rnietourena@safareyes.es>
2023-01-11 20:26:16 +0100

    haciendo conexion bbdd repository, entity y controller

---
+++ src/Controller/SaludoController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MensajeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class SaludoController extends AbstractController
+{
+    #[Route('/mensaje', name: 'app_mensaje')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SaludoController.php',
+        ]);
+    }
+    #[Route('/mensaje/list', name: 'app_mensaje')]
+    public function listar(MensajeRepository $mensajeRepository): JsonResponse
+    {
+        $listLogin = $mensajeRepository->findAll();
+
+        $listJson = $this->toJson($listLogin);
+
+        return new JsonResponse($listJson, 200, [], true);
+
+    }
+}
---
+++ src/Entity/Login.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+use App\Repository\LoginRepository;
+use DateTime;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Integer;
+
+#[ORM\Entity(repositoryClass: LoginRepository::class)]
+class Login
+{
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?Integer $rol = null;
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
