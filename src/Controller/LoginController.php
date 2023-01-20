d905bc9fac0f1a84b02dc3ae7c5d923b37832fbe
Biasem <rnietourena@safareyes.es>
2023-01-20 17:14:12 +0100

    borrado jms serializer bundle

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,16 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Login;
 use App\Repository\LoginRepository;
-use App\Utils\JsonResponseConverter;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -40,7 +35,6 @@ class LoginController extends AbstractController
 
         //Obtener Json del body
         $json  = json_decode($request->getContent(), true);
-        echo "hola";
         //CREAR NUEVO USUARIO A PARTIR DEL JSON
         $loginNuevo = new Login();
 
--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -3,17 +3,17 @@
 namespace App\Controller;
 
 use App\Repository\UsuarioRepository;
-use App\Utils\JsonResponseConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 
 
 class UsuarioController extends AbstractController
 {
-    #[Route('/usuario', name: 'usuario')]
+    #[Route('/usuario/list', name: 'usuarioListar')]
 public function listar(UsuarioRepository $usuarioRepository): JsonResponse
 {
 
@@ -28,6 +28,26 @@ public function listar(UsuarioRepository $usuarioRepository): JsonResponse
 //    $listJson = $jsonConverter->toJson($listLogin);
 //    return new JsonResponse($listJson, 200, [], true);
 }
+    #[Route('/usuario/buscar', name: 'app_usuario_buscar', methods: ['GET'])]
+    public function buscarPorNombre(UsuarioRepository $usuarioRepository,
+                                    Request $request): JsonResponse
+    {
+        $nombre = $request->query->get("usuario");
+
+        $parametrosBusqueda = array(
+            'username' => $nombre
+        );
+
+        $listUsuarios = $usuarioRepository->findBy($parametrosBusqueda);
+
+//        $listJson = $utilidades->toJson($listUsuarios);
+
+        return $this->json($listUsuarios, 200, [], [
+            AbstractNormalizer::IGNORED_ATTRIBUTES => ['__initializer__', '__cloner__', '__isInitialized__'],
+        ]);
+
+    }
+
 
 
 
