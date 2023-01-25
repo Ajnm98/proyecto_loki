3e6d7d443ba217bb24535557bbe674146c5b38d1
Antonito <anunezmorejon@safareyes.es>
2023-01-25 19:50:57 +0100

    Chat abiertos finish

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ChatRepository;
 use App\Utils\ArraySort;
 use App\Utils\JsonResponseConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -49,6 +50,56 @@ class ChatController extends AbstractController
 
         return $this->json($listArraySort);
 
+    }
+
+    #[Route('/chat/listachatsUsuario', name: 'chat_usuario',  methods: ['GET', 'HEAD'])]
+    public function listarchatsAbiertosUsuario(Request $request, ChatRepository $chatRepository): JsonResponse
+    {
+
+        $id = $request->query->get("usuario_id");
+
+
+        $array = array();
+
+        $parametrosBusqueda = array(
+            'usuario_id_emisor' => $id
+        );
+
+        $parametrosBusqueda2 = array(
+            'usuario_id_receptor' => $id
+        );
+
+        $listChats1 = $chatRepository->findBy($parametrosBusqueda, []);
+        $listChats2 = $chatRepository->findBy($parametrosBusqueda2, []);
+
+        $listChats = array_merge($listChats1, $listChats2);
+
+        foreach ($listChats as $chat){
+            $parametrosBusqueda3 = array(
+                'id' => $id
+            );
+
+            if($chat->getUsuarioIdEmisor()->getId()!=$parametrosBusqueda3){
+                $chat1 = $chat->getUsuarioIdEmisor();
+                array_push($array, $chat1);
+            }
+            else{
+                $chat2 = $chat->getUsuarioIdReceptor();
+                array_push($array, $chat2);
+            }
+
+        }
+
+        for($i = 0; $i < count($array); ++$i) {
+            if ($id = $array[$i]->getId()) {
+                unset($array[$i]);
+            }
+        }
+
+
+        return $this->json($array, 200, [], [
+            AbstractNormalizer::IGNORED_ATTRIBUTES => ['__initializer__', '__cloner__', '__isInitialized__'],
+        ]);
     }
 
 
