aec928c37626d7afa99c206b3b229f257bb20aa8
Biasem <rnietourena@safareyes.es>
2023-01-25 19:32:48 +0100

    borrado publicacion y respuesta

--- src/Controller/PublicacionController.php
+++ src/Controller/PublicacionController.php
@@ -6,9 +6,11 @@ use App\Entity\Usuario;
 use App\Repository\AmigosRepository;
 use App\Repository\ChatRepository;
 use App\Repository\PublicacionRepository;
+use App\Repository\RespuestaRepository;
 use App\Repository\UsuarioRepository;
 use App\Utils\ArraySort;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
@@ -17,7 +19,7 @@ class PublicacionController extends AbstractController
 {
 
     #[Route('/publicacion', name: 'publicacion')]
-    public function listarpublicacion(PublicacionRepository $publicacionRepository)//: JsonResponse
+    public function listarpublicacion(PublicacionRepository $publicacionRepository): JsonResponse
     {
         $listPublicacion = $publicacionRepository->findAll();
         return $this->json($listPublicacion, 200, [], [
@@ -27,7 +29,7 @@ class PublicacionController extends AbstractController
     }
 
     #[Route('/publicaciones/usuario',  methods: ['GET', 'HEAD'])]
-    public function listarPublicacionUsuario(Request $request, PublicacionRepository $publicacionRepository)//: JsonResponse
+    public function listarPublicacionUsuario(Request $request, PublicacionRepository $publicacionRepository): JsonResponse
     {
 
         $id = $request->query->get("id");
@@ -38,6 +40,24 @@ class PublicacionController extends AbstractController
         $listPublicacion1 = $publicacionRepository->findBy($parametrosBusqueda);
         return $this->json($listPublicacion1);
     }
+
+
+    //BORRA PUBLICACION CON LAS RESPUESTAS ASOCIADAS
+    #[Route('/publicacion/delete', name: 'publicacion_delete', methods: ['POST'])]
+    public function delete(Request $request,PublicacionRepository $publicacionRepository,RespuestaRepository $respuestaRepository): JsonResponse
+    {
+
+        //Obtener Json del body
+        $json  = json_decode($request->getContent(), true);
+
+        $id = $json['id'];
+        $respuestaRepository->borrarTodasRespuestasPorPublicacion($id);
+        $publicacionRepository->borrarPublicacion($id);
+
+        return new JsonResponse("{ mensaje: Publicacion borrada correctamente }", 200, [], true);
+
+    }
+
 
 
 }
